467d774e88a787415f37a6c8b7900aa4adb14a44
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-08-10 11:21:37 +0100

    created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 
+// Bootstrap Components
+import Form from 'react-bootstrap/Form';
 
 function App() {
 
   return (
     <div>
-      
+      <h1 className="logo">TinyRoute</h1>
+      <p className="tagline">Say goodbye to cumbersome, lengthy URLs and hello to elegant simplicity.</p>
+
+      <div className="form-container">
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Website url</Form.Label>
+          <Form.Control type="url" className='input' size='lg' placeholder="https://website.com" />
+          </Form.Group>
+
+        <div className="btn-container"> 
+          <button type='button' className='submit-btn'>Shorten</button>
+        </div>
+        </Form>
+      </div>
     </div>
   )
 }
